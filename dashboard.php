cd8aadaff2ca0b10da718b615980891c88686416
fa12id <ridz.mcpack@gmail.com>
2025-09-29 11:06:55 +0700

    Delete dashboard.php

--- dashboard.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'auth.php';
-if (!isset($_SESSION['user'])) {
-    header('Location: index.php');
-    exit;
-}
-/* simply output the original todo.html so the user can use the app */
-readfile('todo.html');
-?>
